2c065d8b2a18bb0d6de053bb475a7ea497da4bd1
DARKWOLF2517 <jerrichopamisa@gmail.com>
2023-12-02 08:59:39 +0800

    sdfsd

--- routes/web.php
+++ routes/web.php
@@ -73,9 +73,9 @@ Route::get('/violation1_list', function () {
 //Violation Officer ROUTES
 
 
-Route::get('/violation_records', function () {
-    return view('violation_officer.violation_officer_violation_records');
-});
+// Route::get('/violation_records', function () {
+//     return view('violation_officer.violation_officer_violation_records');
+// });
 
 
 Route::post('/authenticate_user', [LoginController::class, 'authenticate']);
